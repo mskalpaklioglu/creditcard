3948958e1eed54e5200866156d9a87f920b83cc8
Muhammet Şükrü Kalpaklıoğlu <77413367+mskalpaklioglu@users.noreply.github.com>
2023-08-25 09:50:20 +0300

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {ReactiveFormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
+import { CardformComponent } from './cardform/cardform.component';
+import { InputComponent } from './input/input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardformComponent,
+    InputComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
